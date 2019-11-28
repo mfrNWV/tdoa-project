cada178991c5d7e98424143c4a0ec1e6263a8271
mfrNWV <francani.mateus@gmail.com>
2019-11-27 21:05:06 -0300

    Corrected print values

--- vad_doa.py
+++ vad_doa.py
@@ -15,7 +15,7 @@ servo.setSpeed(0,80)
 RATE = 48000
 CHANNELS = 4
 VAD_FRAMES = 10     # ms
-DOA_FRAMES = 200    # ms
+DOA_FRAMES = 100    # ms
 
 
 def main():
@@ -28,11 +28,12 @@ def main():
     doa_chunks = 128
 
     try:
-        with MicArray(RATE, CHANNELS, )  as mic:
+        with MicArray(RATE, CHANNELS, RATE * VAD_FRAMES /1000)  as mic:
+            # chunck size com doa_chuncks para o caso sem vad
             for chunk in mic.read_chunks():
                 # Use single channel audio to detect voice activity
-                # if vad.is_speech(chunk[0::CHANNELS].tobytes(), RATE):
-                    # speech_count += 1
+                if vad.is_speech(chunk[0::CHANNELS].tobytes(), RATE):
+                    speech_count += 1
                     # sys.stdout.write('1')
                 # else:
                     # sys.stdout.write('0')
@@ -40,14 +41,14 @@ def main():
                 # sys.stdout.flush()
 
                 chunks.append(chunk)
-                if len(chunks) >= doa_chunks:
-                     #if speech_count > (doa_chunks / 2):
-                    frames = np.concatenate(chunks)
-                    direction = mic.get_direction(frames)
-                    servodir = int(6272 + 45.5*direction)
-                    servo.setTarget(0,servodir)
-                    #pixel_ring.set_direction(direction)
-                    print('\n{}'.format(int(direction)))
+                if len(chunks) == doa_chunks:
+                    if speech_count > (doa_chunks / 2):
+                        frames = np.concatenate(chunks)
+                        horizontal, vertical = mic.get_direction(frames)
+                        servodir = int(6273 - 45.5*horizontal)
+                        servo.setTarget(0,servodir)
+                        #pixel_ring.set_direction(direction)
+                        print('\n H:{h} V:{v}'.format(h = int(horizontal), v = int(vertical)))
 
                     speech_count = 0
                     chunks = []
